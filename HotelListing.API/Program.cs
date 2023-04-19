b79b020f7d92d27d89d896d6e4ab3f45b0ff67f4
Evan Malherbe <evan@ix.co.za>
2023-04-19 15:36:09 +0200

    Added caching.

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
 using Serilog;
 using System.Security.Cryptography.Xml;
 using System.Text;
@@ -89,6 +90,12 @@ builder.Services.AddAuthentication(options =>
 	};
 });
 
+builder.Services.AddResponseCaching(options =>
+{
+	options.MaximumBodySize = 1024;
+	options.UseCaseSensitivePaths = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -105,6 +112,19 @@ app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
+
+app.UseResponseCaching();
+
+app.Use(async (context, next) =>
+{
+	context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue()
+	{
+		Public = true,
+		MaxAge = TimeSpan.FromSeconds(10)
+	};
+	context.Response.Headers[HeaderNames.Vary] = new string[] { "Accept-Encoding" };
+	await next();
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
